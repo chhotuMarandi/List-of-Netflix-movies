70f503527011cc879795edce34ebe695f0fe6772
chhotu marandi <cmarandi29@gmail.com>
2022-07-31 11:58:44 +0530

    add basic style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="main-container">
       <h1>List Of Top Five Netflix Series</h1>
       <div className="container">
         <div className="card">
@@ -12,7 +13,10 @@ const App = () => {
           />
           <p className="platform">Netflix Original Series</p>
           <h3 className="title">Stranger Things</h3>
-          <button>Watch Now</button>
+          <button>
+            <a href="https://www.netflix.com/in/title/80057281"></a>
+            Watch Now
+          </button>
         </div>
       </div>
     </div>
