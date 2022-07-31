a1e949abbcdd439192e920b49db4f98cfaaea286
chhotu marandi <cmarandi29@gmail.com>
2022-07-31 11:43:18 +0530

    basic app structure is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Hello Chhotu</h1>;
+  return (
+    <div>
+      <h1>List Of Top Five Netflix Series</h1>
+      <div className="container">
+        <div className="card">
+          <img
+            src="https://assets-prd.ignimgs.com/2022/05/12/stranger-things-4-poster-1652364986162.jpeg"
+            alt=""
+          />
+          <p className="platform">Netflix Original Series</p>
+          <h3 className="title">Stranger Things</h3>
+          <button>Watch Now</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
