aaf25798f9f1dc19ead58e7707292e56dd4b7e7e
chhotu marandi <cmarandi29@gmail.com>
2022-08-02 10:28:32 +0530

    add data of serie component

---
+++ src/Components/SeriesList.js
@@ -0,0 +1,8 @@
+const SeriesList = [
+  {
+    img: '"https://assets-prd.ignimgs.com/2022/05/12/stranger-things-4-poster-1652364986162.jpeg"',
+    platform: 'Netflix Original Series',
+    title: 'Stranger Things',
+    link: '"https://www.netflix.com/in/title/80057281"',
+  },
+];
